0854e8a65d3e475c747d23848a8be57955b0b215
Jon Jessee <tjessee7624@gmail.com>
2020-10-19 10:51:30 -0600

    Built ORM queries for the application.

--- config/orm.js
+++ config/orm.js
@@ -1,18 +1,32 @@
-//selectAll()
-//insertOne()
-//updateOne()
+const connection = require('./connection');
 
 const orm = {
-    selectAll: function() {
-
+    selectAll: function(tableName) {
+        let queryString = 'SELECT * FROM ??';
+        connection.query(queryString, [tableName], (err, res) => {
+            if(err) throw err;
+            console.log(res);
+        });
     },
 
-    insertOne: function() {
-
+    insertOne: function(name) {
+       let queryString = 'INSERT INTO burgers SET ??';
+       connection.query(queryString,
+        {
+            burger_name: name
+        },
+        (err, res) => {
+            if(err) throw err;
+            console.log(res);
+        }) 
     },
 
-    updateOne: function() {
-
+    updateOne: function(boolean, idCol, id) {
+        let queryString = 'UPDATE burgers SET ?? WHERE ?? = ?';
+        connection.query(queryString, [{devoured: true}, idCol, id], (err, res) => {
+            if(err) throw err;
+            console.log(res);
+        });
     }
 };
 
