313c5f34d4c05cca7311eeadb1d18a72ea6f2278
Jon Jessee <tjessee7624@gmail.com>
2020-10-21 12:00:04 -0600

    Moved .env file to root of file and created test commands in package.json.

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,15 +5,21 @@ const burger = require('../models/burger');
 
 //Server routes
 router.get('/', (req, res) => {
-    
+   burger.all((data) => {
+       let burgerObj = {
+           burgers: data
+       };
+       console.log(burgerObj);
+       res.render('index', burgerObj);
+   });
 });
 
-router.post('api/burgers', (req, res) => {
+// router.post('api/burgers', (req, res) => {
     
-});
+// });
 
-router.put('/api/burgers/:id', (req, res) => {
+// router.put('/api/burgers/:id', (req, res) => {
 
-})
+// })
 
 module.exports = router;
